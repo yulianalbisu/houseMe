c9641ea51ffcb8775eb725dc5289660592c06c17
yulianalbisu <ya.aldrich@yahoo.com>
2023-12-13 19:09:54 -0500

    Initial Commit

---
+++ houseMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  houseMe
+//
+//  Created by yuliana aldrich on 12/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ houseMe/houseMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  houseMeApp.swift
+//  houseMe
+//
+//  Created by yuliana aldrich on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct houseMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
